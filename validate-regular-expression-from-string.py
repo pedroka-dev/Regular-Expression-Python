821941fc85ec764cbe6d224978fe9a5acd62c073
Pedroca <pedro.henric.rosario@gmail.com>
2021-12-04 21:22:41 -0300

    Tests with expected matches

--- validate-regular-expression-from-string.py
+++ validate-regular-expression-from-string.py
@@ -1,7 +1,6 @@
 import re
 
 # Method
-
 def isValidExpression(regexInput,string):
     match = re.fullmatch(regexInput, string, re.MULTILINE)
     if(match != None):
@@ -9,3 +8,40 @@ def isValidExpression(regexInput,string):
         return True
     else:
         return False
+
+
+# Tests
+regex = r"^(?![a-z])[A-Z]([(a-z)|( )|(,)]*)([.]|[!]|[?])$"
+
+def valitateStringList(listOfStrings):
+    for expression in listOfStrings:
+        if(isValidExpression(regex,expression)):
+            print("---Test passed for expression: '" + expression + "'")
+        else:
+            print("!!!Test failed. Invalid expression '" + expression + "' is not compatible with regex input.")
+
+valid_strings_list = [
+    "Be kind, for everyone you meet is fighting a harder battle.",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    "Aaaaaaaaaaaaa.",
+    "Aaaaaaaaaaaaa!",
+    "Aaaaaaaaaaaaa?",
+    "Aaaaaaa aaaaa aaaaaaa.",
+    "Asafsafsafassaffsaafssfaasf.",
+    "Aaaaaa,aaaaaaa.",
+]
+
+invalid_strings_list = [
+    "Invalid because cammelCase.",
+    "INVALID BECAUSE UPPERCASE.",
+    "Invalid because mbers like 1 to 9.",
+    "Invalid because multiple dots...",
+    "Invalid because multiple exclamations!!!",
+    "Invalid because numbers like 1 to 9..",
+    "Invalid because #@#&*+- characters.",
+    "Invalid because áóéíãõââ characters.",
+    "Invalid because áóéíãõââ characters.",
+]
+
+valitateStringList(valid_strings_list)
+valitateStringList(invalid_strings_list)
